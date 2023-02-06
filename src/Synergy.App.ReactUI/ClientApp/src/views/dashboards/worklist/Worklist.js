1311782b3c8dc438e7630f5640dc3ab95e367afa
rajkumar <rajkumar.sen@xtranetindia.com>
2023-02-06 18:55:03 +0530

    leavebtn

--- src/Synergy.App.ReactUI/ClientApp/src/views/dashboards/worklist/Worklist.js
+++ src/Synergy.App.ReactUI/ClientApp/src/views/dashboards/worklist/Worklist.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import * as React from 'react';
+import WorklistTask from './WorklistTask'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
-
-
-function Worklist() {
+export default function BoxSx() {
   return (
-    <div>hello</div>
-  )
-}
+<>
 
-export default Worklist
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+    WorkList
+  </Typography>
+    <Box
+      sx={{
+        width: 999,
+        height: '30px',
+        backgroundColor: 'primary.dark',
+       
+      }}
+    />
+
+
+
+
+
+
+
+    </>
+  );
+}
